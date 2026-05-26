5cf66fd983066b259a27416d5d2b6d82cf52454f
Th0rN13 <th0r@inbox.ru>
2026-05-27 01:05:17 +0500

    feat: use unplugin icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 import { playwright } from "@vitest/browser-playwright";
 import { sveltekit } from "@sveltejs/kit/vite";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), Icons({ compiler: "svelte" })],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
